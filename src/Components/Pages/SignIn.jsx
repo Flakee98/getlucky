46d8bce7aa8ee685b1b9e04cb0fd9aa130a51b0c
Miras Mujeeb <mirasmujeeb@Mirass-MacBook-Pro.local>
2023-08-06 15:48:44 +0530

    login

--- src/Components/Pages/SignIn.jsx
+++ src/Components/Pages/SignIn.jsx
@@ -1,95 +1,114 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
-import 'react-phone-input-2/lib/style.css';
-import PhoneInput from 'react-phone-input-2';
+import "react-phone-input-2/lib/style.css";
+import PhoneInput from "react-phone-input-2";
 function Signin() {
-    const [clickEmail, setClickEmail] =useState(true)
-    const [clickMobile, setClickMobile] =useState(false)
-    const [phoneNumber, setPhoneNumber] = useState('');
+  const [clickEmail, setClickEmail] = useState(true);
+  const [clickMobile, setClickMobile] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-const EmailClick =()=>{
-    setClickEmail(true)
-    setClickMobile(false)
-}
+  const EmailClick = () => {
+    setClickEmail(true);
+    setClickMobile(false);
+  };
 
-const MobilelClick =()=>{
-
-    setClickMobile(true)
-    setClickEmail(false)
-}
-const handlePhoneNumberChange = (value) => {
+  const MobilelClick = () => {
+    setClickMobile(true);
+    setClickEmail(false);
+  };
+  const handlePhoneNumberChange = (value) => {
     setPhoneNumber(value);
   };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // Do something with the phone number (with country code)
-    console.log('Phone Number:', phoneNumber);
+    console.log("Phone Number:", phoneNumber);
   };
 
   return (
-    
     <div>
-    <Container>
-      <Row className="vh-100 d-flex justify-content-center align-items-center" >
-        <Col md={8} lg={6} xs={12}>
-          <div className="border border-3 border-primary"></div>
-          <Card className="shadow" style={{backgroundColor: "#102042"}}>
-            <Card.Body>
-              <div className="mb-3 mt-md-4">
-                <h2 className="fw-bold mb-2 text-uppercase  text-white  text-center">SIGIN IN</h2>
-                <div className="mb-3">
-                  <Form className='text-white'>
-
-                  <div class="row">
-                    
-                    <Button variant="primary bg-white text-dark " className="mb-3 col" onClick={ EmailClick}>Email</Button>{' '}
-                     
-                    <Button variant="primary" className="mb-3 col" onClick={ MobilelClick}>Mobile</Button>{' '}
-            
-                          
-                    </div>
-                    {clickEmail  &&(
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow" style={{ backgroundColor: "#102042" }}>
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 mb-5 text-white  text-center">
+                    Sign in
+                  </h2>
+                  <div className="mb-3">
+                    <Form className="text-white">
+                      <div class="row mx-1">
+                        <Button
+                          variant={
+                            "primary " +
+                            (clickEmail === true ? "bg-light text-dark" : "")
+                          }
+                          className="mb-3 col"
+                          onClick={EmailClick}
+                        >
+                          Email
+                        </Button>{" "}
+                        <Button
+                          variant={
+                            "primary   " +
+                            (clickMobile === true ? "bg-light text-dark" : "" ) 
+                           
+                          }
+                          className="mb-3 col"
+                          onClick={MobilelClick}
+                        >
+                          Mobile
+                        </Button>{" "}
+                      </div>
+                      {clickEmail && (
                         <>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                      <Form.Label>Email ID *</Form.Label>
-                      <Form.Control  type="email" placeholder="Email Id" />
-                    </Form.Group>
-
-                    </>
-
-                    )}
-                     {clickMobile  &&(
+                          <Form.Group
+                            className="mb-3"
+                            controlId="formBasicPassword"
+                          >
+                            <Form.Label>Email ID *</Form.Label>
+                            <Form.Control type="email" placeholder="Email Id" />
+                          </Form.Group>
+                        </>
+                      )}
+                      {clickMobile && (
                         <>
-                   <Form.Group controlId="formGroupPhoneNumber"className="mb-3" >
-              <Form.Label>Mobile No *</Form.Label>
-              <PhoneInput
-                inputStyle={{ width: '100%' }}
-                country={'us'}
-                value={phoneNumber}
-              />
-            </Form.Group>
+                          <Form.Group
+                            controlId="formGroupPhoneNumber"
+                            className="mb-3"
+                          >
+                            <Form.Label>Mobile No *</Form.Label>
+                            <PhoneInput
+                              inputStyle={{ width: "100%" }}
+                              country={"us"}
+                              value={phoneNumber}
+                            />
+                          </Form.Group>
+                        </>
+                      )}
+                      <Form.Group
+                        className="mb-3 "
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Password *</Form.Label>
+                        <Form.Control type="password" placeholder="Password " />
+                      </Form.Group>
 
-                    </>
-
-                    )}
-                        <Form.Group className="mb-3 " controlId="formBasicPassword">
-                      <Form.Label>Password *</Form.Label>
-                      <Form.Control type="password" placeholder="Password " />
-                    </Form.Group>
-
-                  <div className='row'>
-                    <Form.Group
-                      className="mb-3 col"
-                      controlId="formBasicCheckbox"
-                    >
-                      <p className="small">
-                        <a className="text-primary" href="#!">
-                          Forgot password?
-                        </a>
-                      </p>
-                    </Form.Group>
-{/* 
+                      <div className="row">
+                        <Form.Group
+                          className="mb-3 col"
+                          controlId="formBasicCheckbox"
+                        >
+                          <p className="small">
+                            <a className="text-primary" href="#!">
+                              Forgot password?
+                            </a>
+                          </p>
+                        </Form.Group>
+                        {/* 
                     <Form.Group
                       className="mb-3 col"
                       controlId="formBasicCheckbox"
@@ -100,32 +119,32 @@ const handleSubmit = (e) => {
                         </a>
                       </p>
                     </Form.Group> */}
-                    </div>
+                      </div>
 
-                    <div className="d-grid">
-                      <Button variant="primary" type="submit">
-                        Sign In
-                      </Button>
-                    </div>
-                  </Form>
+                      <div className="d-grid">
+                        <Button variant="primary" type="submit">
+                          Sign In
+                        </Button>
+                      </div>
+                    </Form>
 
-                  <div className="mt-3">
-                    <p className="mb-0  text-center text-white">
-                    Don't have an account ? {" "}
-                      <a href="/signup" className="text-primary fw-bold">
-                       Sign Up
-                      </a>
-                    </p>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center text-white">
+                        Don't have an account ?{" "}
+                        <a href="/signup" className="text-primary fw-bold">
+                          Sign Up
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-  )
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Signin
+export default Signin;
--- src/Components/Pages/Winners.jsx
+++ src/Components/Pages/Winners.jsx
@@ -1,22 +1,250 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap';
+import React from "react";
+import { Card, Button, Row, Col, Container } from "react-bootstrap";
 
 function Winners() {
   return (
-    <div className='d-flex align-items-center flex-column' style={{ width: "100%", height: '100%' }}>
-    <h1 style={{color: "#142D62"}}>Past Draw Winners</h1>
-    <Card  style={{ width: "50%", height: '10vh'}}>
-  <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
-    <div>
-      <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+    <div
+      className="d-flex align-items-center flex-column my-5 pt-5"
+      style={{ width: "100%", minHeight: "80vh" }}
+    >
+      <h2 className="mt-4" style={{ color: "#fff" }}>Past Draw Winners</h2>
+      <Container>
+        <Row className="w-100">
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Card className="m-3 ms-0" style={{ width: "100%", height: "10vh" }}>
+              <Card.Body
+                className="d-flex align-items-center justify-content-between"
+                style={{ backgroundColor: "#142d62", borderRadius: "6px" }}
+              >
+                <div>
+                  <Card.Text style={{ color: "white" }}>
+                    Tri-Daily Draw #067
+                  </Card.Text>
+                </div>
+                <div>
+                  <Button
+                    style={{ color: "black", backgroundColor: "#FFFFFF" }}
+                  >
+                    WINNERS
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>{" "}
+      </Container>
     </div>
-    <div>
-      <Button style={{color: "black", backgroundColor: "#FFFFFF" }}>WINNERS</Button>
-    </div>
-  </Card.Body>
-</Card>
-</div>
-  )
+  );
 }
 
-export default Winners
+export default Winners;
