1bd483842f9e26f8db648ece8332b61c5326b60c
Miras Mujeeb <mirasmujeeb@Mirass-MacBook-Pro.local>
2023-08-06 15:01:13 +0530

    changes

--- src/Components/Pages/PastDrawResult.jsx
+++ src/Components/Pages/PastDrawResult.jsx
@@ -1,20 +1,192 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 
 function PastDrawResult() {
   return (
-    <div className='d-flex align-items-center flex-column' style={{ width: "100%", height: '100%' }}>
-        <h1 style={{color: "#142D62"}}>Past Draw Results</h1>
-        <Card  style={{ width: "50%", height: '10vh'}}>
+    <div className='d-flex mt-5 pt-5 align-items-center flex-column' style={{ width: "100%", minHeight: '80vh' }}>
+        <h2 className='mt-5 mb-5' style={{color: "#fff"}}>Past Draw Results</h2>
+        <Container className='d-flex justify-content-center mb-5'>
+
+       <Row className='w-100'>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
       <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
         <div>
           <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
         </div>
         <div>
-          <Button style={{color: "black", backgroundColor: "#FFFFFF" }}>RESULTS</Button>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
         </div>
       </Card.Body>
-    </Card>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+        <Col md={4} sm={6} xs={12}>
+           <Card className='m-3 ms-0 w-100' style={{ width: "50%", height: '10vh'}}>
+      <Card.Body className="d-flex align-items-center justify-content-between" style={{backgroundColor: "#142d62", borderRadius: "6px"}}>
+        <div>
+          <Card.Text style={{color: "white"}}>Tri-Daily Draw #067</Card.Text>
+        </div>
+        <div>
+          <Button className='px-4' style={{color: "black", backgroundColor: "#FFFFFF" }}>
+            <span style={{
+              fontSize: "14px",
+            }}>Results</span>
+          </Button>
+        </div>
+      </Card.Body>
+    </Card> 
+        </Col>
+       </Row>
+       
+    </Container>
     </div>
   )
 }
